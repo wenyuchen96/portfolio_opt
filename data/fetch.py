c8a9663a76086b658ca7c4258228b8f24f8abc00
Wenyu Chen <wenyuc21@gmail.com>
2025-09-11 20:58:39 -0400

    delete extra test code in data/fetch.py

--- data/fetch.py
+++ data/fetch.py
@@ -5,6 +5,3 @@ def get_stock_data(tickers: list[str], start_date: str, end_date: str):
     # Fetch historical adjusting closing price (auto_adjust=True) for a list of tickers.
     data = yf.download(tickers, start=start_date, end=end_date)['Close']
     return data.dropna()
-
-df = get_stock_data(['AAPL', 'MSFT', 'AMZN'], '2022-01-01', '2022-12-31')
-print(df)
