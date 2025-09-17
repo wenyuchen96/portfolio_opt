2032ab77cf96fb32d97756ac99797169007f6071
Wenyu Chen <wenyuc21@gmail.com>
2025-09-17 14:44:40 -0400

    completed base_optimizer.py

--- port_opt/base_optimizer.py
+++ port_opt/base_optimizer.py
@@ -3,7 +3,7 @@ import copy
 import json
 import warnings
 from collections.abc import Iterable
-from typing import List
+from typing import List, Callable
 
 import cvxpy as cp
 import numpy as np
@@ -143,6 +143,185 @@ class BaseConvexOptimizer(BaseOptimizer):
 
     def deepcopy(self):
         """
-        returns a custome deep copy of the optimizer
+        returns a custom deep copy of the optimizer, as cvxpy does not support deepcopy
         """
         self_copy = copy.copy(self)
+        self_copy._additional_objectives = [
+            copy.copy(obj) for obj in self._additional_objectives        
+        ]
+        self_copy._constraints = [
+            copy.copy(constraint) for constraint in self._constraints
+        ]
+        return self._copy
+    
+    def _map_bounds_to_constraints(self, test_bounds: tuple | tuple[float, float]):
+        """
+        Convert input bound into a form acceptable bt cvxpy and add to the constraints list.
+
+        test_bounds: minimum and maximum weight of each asset OR single min/max pair if all identical OR pairs of arrays corresponding to lower/upper bounds; default to (0,1)
+        """
+
+        if len(test_bounds) == self.n_assets and not isinstance(test_bounds[0], (float, int)):
+            bounds = np.array(test_bounds, dtype=float)
+            self._lower_bounds = np.nan_to_num(bounds[:, 0], nan=-np.inf)
+            self._upper_bounds = np.nan_to_num(bounds[:, 1], nan=np.inf)
+        else:
+            #otherwise this must be a pair
+            if len(test_bounds) != 2 or not isinstance(test_bounds, (tuple, list)):
+                raise TypeError(
+                    "test_bounds must be a pair (lower bound, upper bound) OR a collection of bounds for each asset"
+                )
+            lower, upper = test_bounds
+
+            #replace None values with appropriate +/- 1
+            if np.isscalar(lower) or lower is None:
+                lower = -1 if lower is None else lower
+                self._lower_bounds = np.array([lower] * self.n_assets)
+                upper = 1 if upper is None else upper
+                self._upper_bounds = np.array([upper] * self.n_assets)
+            else: #1 array represents all the lower bounds, and 1 array represents all the upper bounds
+                self._lower_bounds = np.nan_to_num(lower, nan=-1)
+                self._upper_bounds = np.nan_to_num(upper, nan=1)
+
+        self.add_constraint(lambda w: w >= self._lower_bounds)
+        self.add_constraint(lambda w: w <= self._upper_bounds)
+
+    def is_parameter_defined(self, parameter_name: str) -> bool:
+        """
+        Check if a parameter is defined
+        """
+        is_defined = False
+        objective_and_constraints = (
+            self._constraints + [self._objective]
+            if self._objective is not None
+            else self._constraints
+        )
+
+        for expr in objective_and_constraints:
+            params = [arg for arg in _get_all_args(expr) if isinstance(arg, cp.Parameter)]
+            for param in params:
+                if param.name() == parameter_name and not is_defined:
+                    is_defined = True
+                elif param.name() == parameter_name and is_defined:
+                    raise exceptions.InstantiationError(
+                        "Parameter name defined multiple times"
+                    )
+        return is_defined
+
+    
+    def _get_all_args(expression: cp.Expression) -> List[cp.Expression]:
+        """
+        Helper function to recursively get all arguments from a cvxpy expression.
+        """
+
+        if expression.args == []:
+            return [expression]
+        else:
+            return list(_flatten([_get_all_args(arg) for arg in expression.args]))
+        
+    def _flatten(alist: Iterable) -> Iterable:
+        for item in alist:
+            if isinstance(item, Iterable) and not isinstance(item, (str, bytes)):
+                yield from _flatten(item)
+            else:
+                yield item
+
+    def update_parameter_value(self, parameter_name: str, new_value: float) -> None:
+        """
+        Update the value of a parameter.
+        """
+        if not self.is_parameter_defined(parameter_name):
+            raise exceptions.InstantiationError(
+                f"Parameter {parameter_name} was not defined"
+            )
+        was_updated = False
+        objective_and_constraints = (
+            self._constraints + [self._objective]
+            if self._objective is not None
+            else self._constraints
+        )
+        for expr in objective_and_constraints:
+            params = [arg for arg in _get_all_args(expr) if isinstance(arg, cp.Parameter)]
+            for param in params:
+                if param.name() == parameter_name:
+                    param.value = new_value
+                    was_updated = True
+        if not was_updated:
+            raise exceptions.instantiationError(
+                f"Parameter {parameter_name} was not updated"
+            )
+    
+    def _solve_cvxpy_opt_problem(self):
+        """
+        Helper method to solve the cvxpy problem and check output, once objectives and constraints have been defined
+        """
+        try:
+            if self._opt is None:
+                self._opt = cp.Problem(cp.Minimize(self._objective), self._constraints)
+                self._initial_objective = self._opt.objective.id
+                self._initial_constraint_ids = (const.id for const in self._constraints)
+            else:
+                if not self._objective.id == self._initial_objective:
+                    raise exceptions.InstantiationError(
+                        "The objective function was changed after the initial optimization"
+                        "Please create a new instance instead"
+                    )
+                
+                constr_ids = {const.id for const in self._constraints}
+                if not constr_ids == self._initial_constraint_ids:
+                    raise exceptions.InstantiationError(
+                        "The constraints were changed after the initial optimization"
+                        "Please create a new instance instead"
+                    )
+            self._opt.solve(
+                solver=self._solver, verbose=self._verbose, **self._solver_options
+            )
+        except (TypeError, cp.DCPError) as e:
+            raise exceptions.OptimizationError from e
+        
+        if self._opt.status not in {"optimal", "optimal_inaccurate"}:
+            raise exceptions.OptimizationError(
+                "Solver status: {}".format(self._opt.status)
+            )
+        self.weights = self._w.value.round(16) + 0.0
+        return self._make_output_weights()
+    
+    def add_objective(self, new_objective: cp.Expression, **kwargs):
+        """
+        Add a new term into the objective function. This must be convex, and built from cvxpy atomic functions.
+        """
+        if self._opt is not None:
+            raise exceptions.InstantiationError(
+                "It's not recommended to add objectives to an already solved problem which might have unintended consequences"
+                "A new instance should be created for the new set of objectives."
+            )
+        self._additional_objectives.append(new_objective(self._w, **kwargs))
+
+    def add_constraint(self, new_constraint: Callable[[cp.Variable], cp.Expression]):
+        """
+        Add a new constraint to the optimization problem. This must satisfy DCP rules.
+        """
+        if not callable(new_constraint):
+            raise TypeError("New constraint must be a function (callable)")
+        if self._opt is not None:
+            raise exceptions.InstantiationError(
+                "Adding constraints to an already solved problem is not supported"
+                "A new instance should be created for the new set of constraints."
+            )
+        self._constraints.append(new_constraint(self._w))
+    
+    def convex_objective(self, custom_objective, weights_sum_to_one=True, **kwargs):
+        """
+        Optimize a custom convex objective function. Constraints should be added with 'ef.add_constraint()'. Optimizer arguments must be passed as keyword-args.
+        """
+
+        #custom_objective must have the right signature(w, **kwargs)
+        self._objective = custom_objective(self._w, **kwargs)
+        
+        for obj in self._additional_objectives:
+            self._objective += obj
+        
+        if weights_sum_to_one:
+            self.add_constraint(lambda w: cp.sum(w) == 1)
+        
+        return self._solve_cvxpy_opt_problem()
